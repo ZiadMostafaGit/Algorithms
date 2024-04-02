4ac4e27cd6b657629dc5f531ede6313808723acd
ZiadMostafaGit <mygithub2003@gmail.com>
2024-04-02 22:34:19 +0200

    update

---
+++ Mostafa_saad_algo/Recursion/leftmax.py
@@ -0,0 +1,19 @@
+def leftmax(arr,pos,maxval,index):
+    if index==len(arr):
+        return 
+    if index<=pos and arr[index]>maxval:
+        maxval=arr[index]
+    elif index>pos:
+            arr[index]=maxval
+
+    leftmax(arr,pos,maxval,index=index+1)
+    return arr
+
+
+
+
+
+
+
+arr=[1,555,3,22,6,88,555,32,511,2,21]
+print(leftmax(arr,3,0,0))
